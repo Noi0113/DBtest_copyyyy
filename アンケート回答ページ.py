a8856d60f25bad35034725314af23f46f82ab609
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-01 14:05:49 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 def main():
     status_area = st.empty()
 #タイトル
@@ -7,18 +8,22 @@ st.title('アンケート回答')
 st.markdown('フォーム')
 
 
+#選択肢はフォームの外に作らないとエラーが出るかも
 univ_options = ['あ','い']#こんな感じで、データベースから大学名のリストを取ってくればプルダウン作成は可能です！！！
+absent_options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']#このリストをmonketsu-option.dbから取得するとよい
 
 # フォームを作成します
 with st.form(key='my_form'):
-    input_taikaiid = st.text_input(label = '大会ID')
-    input_name = st.text_input(label='名前')
-    input_level = st.selectbox('級',options=['A','B','C','D','E'])
-    input_univ = st.selectbox('大学名', options=univ_options)
+    input_taikaiid = st.text_input(label = '大会IDを入力してください')
+    #大会IDはフォーム外のほうがいいかもしれない…？大会IDからuniv_optionsを作成するならその処理はフォーム外になるかも？
+    input_name = st.text_input(label='名前を入力してください')
+    input_level = st.selectbox('級を入力してください',options=['A','B','C','D','E'])
+    input_univ = st.selectbox('大学名を入力してください', options=univ_options)
     #input_feedback = st.text_area(label='フィードバック')
-    st.markdown ('個人IDを作成してください。アンケート結果を編集する際に必要となりますので、お手元にお控え下さい。')
-    input_kojinid = st.text_input(label = '個人ID')
-
+    absent_matches = st.multiselect('欠席する試合(複数選択可)を入力してください', absent_options)
+  
+    #st.markdown ('個人IDを作成してください。アンケート結果を編集する際に必要となりますので、お手元にお控え下さい。')
+    input_kojinid = st.text_input(label = '個人IDを作成してください。アンケート結果を編集する際に必要となりますので、お手元にお控え下さい。')
     #すべての欄を埋めたら送信できるようにもしたい
     #if input_taikaiid in not Null:
     submit_button = st.form_submit_button(label='送信',use_container_width = True)
